e1ba6bfb2e093cc7ae4bbfdb69e71424c4d8f9bd
Javier Rivera <Sunnuke@users.noreply.github.com>
2020-12-13 13:19:29 -0800

    Created (Model)License class and set it to be a Data Table, named licenses

---
+++ relationships/src/main/java/com/practice/relationships/models/License.java
@@ -0,0 +1,89 @@
+package com.practice.relationships.models;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="licenses")
+public class License {
+	
+//	ATTRIBUTES
+	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String number;
+    private Date expirationDate;
+    private String state;
+    @Column(updatable=false)
+    private Date createdAt;
+    private Date updatedAt;
+    @OneToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name="person_id")
+    private Person person;
+    
+//	Constructors
+    public License() {
+        
+    }
+	
+//	METHODS
+	
+	// Id
+	public Long getId() {
+		return id;
+	}
+	
+	// Number
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	
+	// Expiration Date
+	public Date getExpirationDate() {
+		return expirationDate;
+	}
+	public void setExpirationDate(Date expirationDate) {
+		this.expirationDate = expirationDate;
+	}
+	
+	// State
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	
+	// Date/Time
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+	
+	// Relationship
+	// One to One (Person)
+	public Person getPerson() {
+		return person;
+	}
+	public void setPerson(Person person) {
+		this.person = person;
+	}
+    
+}
