16bfc21fc729e9e4091c42cacf195080aee59c4b
Javier Rivera <Sunnuke@users.noreply.github.com>
2020-12-13 13:08:55 -0800

    Created (Model)Person class and set it to be a Data Table, named persons

---
+++ relationships/src/main/java/com/practice/relationships/models/Person.java
@@ -0,0 +1,89 @@
+package com.practice.relationships.models;
+
+import java.util.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="persons")
+public class Person {
+	
+//	ATTRIBUTES
+	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    @Column(updatable=false)
+    private Date createdAt;
+    private Date updatedAt;
+    @OneToOne(mappedBy="person", cascade=CascadeType.ALL, fetch=FetchType.LAZY)
+    private License license;
+    
+//	Constructors
+    public Person() {
+        
+    }
+//	PRESETS
+	
+	@PrePersist
+	private void onCreate() {
+		this.createdAt = new Date();
+	}
+	@PreUpdate
+	private void onUpdate() {
+		this.createdAt = new Date();
+	}
+	
+//	METHODS
+	
+	// Id
+	public Long getId() {
+		return id;
+	}
+
+	// First Name
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	// Last Name
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	// Date/Time
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	// Relationship
+	// One to One
+	public License getLicense() {
+		return license;
+	}
+	public void setLicense(License license) {
+		this.license = license;
+	}
+    
+}
